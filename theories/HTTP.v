6859bb16c5fb07742a2395be85c029c42f566fbf
Thomas Letan <thomas.letan@ssi.gouv.fr>
2019-10-17 16:01:32 +0200

    Implement a safer treatment of the HTTP request's URI

--- theories/HTTP.v
+++ theories/HTTP.v
@@ -38,14 +38,16 @@ Definition response_to_string (res : response) : string :=
 Definition http_request : parser request :=
   Get <$> (str "GET" *> whitespaces *> read_uri <* whitespaces <* str "HTTP/1.1").
 
-(* TODO: use base. *)
+Definition sandbox (base : list directory_id) (req : uri) : uri :=
+  make_uri (List.app base (canonicalize (dirname req))) (filename req).
+
 (* TODO: add a notation in coq-prelude var (x : T) <- p in q *)
 Definition request_handler `{Provide ix FILESYSTEM}
-    (_base : list directory_id) (req : request)
+    (base : list directory_id) (req : request)
   : impure ix response :=
   match req with
   | Get uri =>
-    do let path := uri_to_path uri in
+    do let path := uri_to_path (sandbox base uri) in
        var isf <- is_file path in
        if (isf : bool)
        then do var fd <- open path in
@@ -57,21 +59,21 @@ Definition request_handler `{Provide ix FILESYSTEM}
   end.
 
 Definition tcp_handler `{Provide ix FILESYSTEM}
-    (_base : list directory_id) (req : string)
+    (base : list directory_id) (req : string)
   : impure ix string :=
   response_to_string <$> match parse http_request req with
                          | inl _ => pure (make_response client_error_BadRequest "")
-                         | inr req => request_handler _base req
+                         | inr req => request_handler base req
                          end.
 
-Definition http_server `{Provide ix FILESYSTEM, Provide ix TCP} (_base : list directory_id)
+Definition http_server `{Provide ix FILESYSTEM, Provide ix TCP}
   : impure ix unit :=
   do var server <- new_tcp_socket "localhost:8000" in
      listen_incoming_connection server;
 
      var client <- accept_connection server in
      var req <- read_socket client in
-     var res <- tcp_handler _base req in
+     var res <- tcp_handler [Dirname "opt"; Dirname "praecia"] req in
 
      write_socket client res;
      close_socket client;
